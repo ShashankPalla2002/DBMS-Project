ee48885c5378e60bc46c2e07a51bb00755b610e0
Shashank Palla <shashankpalla44@gmail.com>
2022-11-06 00:27:04 +0530

    template created with html

--- MiniProject/MiniProject/settings.py
+++ MiniProject/MiniProject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'MiniProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'Templates'],
+        'DIRS': ['Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
