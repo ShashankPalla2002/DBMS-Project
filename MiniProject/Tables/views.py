ad334de3ddfb334ddf03c617baba2813523c1cae
Shashank Palla <shashankpalla44@gmail.com>
2022-11-10 17:46:12 +0530

    push

--- MiniProject/Tables/views.py
+++ MiniProject/Tables/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import IPC_Sections
+from .models import Person_Related
+from .models import Government_Related
+from .models import Property_Related
 
-def home(request):         
-    return render (request, 'Tables/home.html')
+def home(request):     
+    persons = Person_Related.objects.all()
+    government = Government_Related.objects.all()
+    property = Property_Related.objects.all()
+
+    return render (request, 'Tables/home.html', {'person' : persons,    'government' : government, 'property' : property})
     
 
     
