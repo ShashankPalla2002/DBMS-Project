fef9207d2534e854a3ce1637980503cf7fcdfa98
Priyanshu Gupta <unofficialwork0@gmail.com>
2022-11-11 11:55:29 +0530

    almost done home-page

--- MiniProject/MiniProject/settings.py
+++ MiniProject/MiniProject/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'MiniProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'CyberSecurity',
+        'NAME': 'Cybersecurity',
         'USER': 'root',
-        'PASSWORD': 'javaminiproject',
+        'PASSWORD': 'Mysql@25',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
